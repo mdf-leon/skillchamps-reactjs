a4805dc90d2d1fe97e08963a656c76ed464184d7
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2020-10-25 20:04:38 -0300

    savestate

--- src/pages/signed/points/index.tsx
+++ src/pages/signed/points/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Sidebar from "../../../components/Sidebar";
-import { Modal } from "components";
-import { DateTime } from "luxon";
+import React, { useState, useEffect } from 'react';
+import Sidebar from '../../../components/Sidebar';
+import { Modal } from 'components';
+import { DateTime } from 'luxon';
 import {
   SideBarDiv,
   TimeDiv,
@@ -12,7 +12,7 @@ import {
   MainDiv,
   ShowTimeInput,
   Blackground,
-} from "./styles";
+} from './styles';
 import {
   Card,
   CardActionArea,
@@ -21,21 +21,22 @@ import {
   CardMedia,
   Button,
   Typography,
-} from "@material-ui/core";
-import { base } from "../../../config/api";
+} from '@material-ui/core';
+import { base } from '../../../config/api';
 import {
   Theme,
   createStyles,
   makeStyles,
   useTheme,
-} from "@material-ui/core/styles";
-import { AiOutlineMinusCircle, AiFillPlusCircle } from "react-icons/ai";
-import classes from "*.module.sass";
+} from '@material-ui/core/styles';
+import { AiOutlineMinusCircle, AiFillPlusCircle } from 'react-icons/ai';
+import classes from '*.module.sass';
+import { Duration } from 'luxon';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     mainCardText: {
-      color: "white",
+      color: 'white',
     },
     numberText: {
       fontWeight: 300,
@@ -46,16 +47,17 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function BeforePoints(props) {
   const classes = useStyles();
   const [penaltiesConf, setPenaltiesConf] = useState<any[]>([]);
-  const [activeModal, setactiveModal] = useState<any>("");
+  const [activeModal, setactiveModal] = useState<any>('');
   const [dataTrial, setDataTrial] = useState<any>({});
   const [dataRider, setDataRider] = useState<any>({});
-  const [time, setTime] = useState<any>("00:00.000");
+  const [baseTime, setbaseTime] = useState<any>();
   const [tempTime, setTempTime] = useState<any>();
+  const [finalTime, setfinalTime] = useState<any>();
 
   const [point, setpoint] = useState<any>({
-    rider_id: localStorage.getItem("ongoing_rider"),
-    trial_id: localStorage.getItem("ongoing_trial"),
-    time: "0",
+    rider_id: localStorage.getItem('ongoing_rider'),
+    trial_id: localStorage.getItem('ongoing_trial'),
+    time: '0',
   });
 
   const [pens, setpens] = useState<any[]>([]);
@@ -64,14 +66,48 @@ export default function BeforePoints(props) {
   //   console.log(pens);
   // }, [pens])
 
+  function stringToMS(tm) {
+    // return tm[1].replace('.', '')
+    return Duration.fromObject({
+      minutes: tm[0],
+      milliseconds: tm[1].replace('.', ''),
+    }).as('milliseconds');
+    // return new Date(ms).toISOString().slice(14, -1);
+  }
+
+  const updateFinalTime = () => {
+    const msBase = baseTime ? Number(stringToMS(baseTime.split(':'))) : 0;
+    console.log(pens);
+    setpoint({ ...point, time: msBase });
+
+    let tempTime = msBase;
+    console.log(tempTime);
+    for (let i = 0; i < penaltiesConf.length; i++) {
+      tempTime += penaltiesConf[i].time_penalty * (pens[i] || 0);
+      console.log(tempTime);
+    }
+    let unformatedFinalTime: string = Duration.fromObject({
+      milliseconds: tempTime,
+    }).toFormat("mm':'S"); // .splice(4, 0, ":")
+    console.log(unformatedFinalTime);
+    // unformatedFinalTime = '0'.repeat(8 - unformatedFinalTime.length) + unformatedFinalTime
+    let milis = unformatedFinalTime.split(':')[1];
+    // console.log(milis);
+    const formatedFinalTime =
+      unformatedFinalTime.substring(0, 3) +
+      '.' + '0'.repeat(5 - milis.length) + milis;
+
+    setfinalTime(formatedFinalTime);
+  };
+
   useEffect(() => {
     let params = {
-      event_id: localStorage.getItem("event_id"),
+      event_id: localStorage.getItem('event_id'),
       rider_id: point.rider_id,
       trial_id: point.trial_id,
     };
     base
-      .get("/managedTrialsList", { params })
+      .get('/managedTrialsList', { params })
       .then((r) => {
         setDataTrial(r.data);
         // console.log(r.data);
@@ -80,7 +116,7 @@ export default function BeforePoints(props) {
         // console.log(er);
       });
     base
-      .get("/managedRidersList", { params })
+      .get('/managedRidersList', { params })
       .then((r) => {
         setDataRider(r.data);
         // console.log(r.data);
@@ -92,7 +128,6 @@ export default function BeforePoints(props) {
       .get(`/managedPenaltyConfsFromTrial`, { params })
       .then((r) => {
         setPenaltiesConf(r.data);
-        console.log(r.data);
       })
       .catch((er) => {
         console.log(er);
@@ -125,37 +160,37 @@ export default function BeforePoints(props) {
     setpoint({ ...point, penalties: temp });
   };
 
-  const penalty = (pen) => (
+  const penalty = (pen, index) => (
     <div>
       <Typography variant="body2" component="p">
         <strong>
-          {pen.id}.{pen.name}
+          {pen.id}. {pen.name} {index}
         </strong>
       </Typography>
 
       <div
         style={{
-          display: "flex",
-          marginTop: "10px",
-          width: "100%",
-          justifyContent: "center",
-          minHeight: "58px",
+          display: 'flex',
+          marginTop: '10px',
+          width: '100%',
+          justifyContent: 'center',
+          minHeight: '58px',
         }}
       >
         <NumberBox>
           <div
             style={{
-              background: "#1976d3",
-              display: "flex",
-              alignItems: "center",
-              width: "50px",
-              justifyContent: "center",
+              background: '#1976d3',
+              display: 'flex',
+              alignItems: 'center',
+              width: '50px',
+              justifyContent: 'center',
             }}
           >
             <RoundButton
               onClick={(e) => {
                 const temp: any[] = [...pens];
-                temp[pen.id] = (temp[pen.id] || 0) - 1;
+                temp[index] = (temp[index] || 0) - 1;
                 setpens(temp);
               }}
             >
@@ -165,10 +200,10 @@ export default function BeforePoints(props) {
 
           <div
             style={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
             }}
           >
             <Typography
@@ -176,23 +211,23 @@ export default function BeforePoints(props) {
               component="p"
               className={classes.numberText}
             >
-              {pens[pen.id] || "none"}
+              {pens[index] || 'none'}
             </Typography>
           </div>
 
           <div
             style={{
-              background: "#1976d3",
-              display: "flex",
-              alignItems: "center",
-              width: "50px",
-              justifyContent: "center",
+              background: '#1976d3',
+              display: 'flex',
+              alignItems: 'center',
+              width: '50px',
+              justifyContent: 'center',
             }}
           >
             <RoundButton
               onClick={(e) => {
                 const temp = [...pens];
-                temp[pen.id] = (temp[pen.id] || 0) + 1;
+                temp[index] = (temp[index] || 0) + 1;
                 setpens(temp);
               }}
             >
@@ -205,7 +240,7 @@ export default function BeforePoints(props) {
   );
 
   const cancel = (
-    <div style={{ textAlign: "left" }}>
+    <div style={{ textAlign: 'left' }}>
       <h3>
         Are you sure you want to cancel
         <br /> the score for this runner?
@@ -213,23 +248,23 @@ export default function BeforePoints(props) {
       <p>current scores will be deleted</p>
       <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: "20px",
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginTop: '20px',
         }}
       >
         <Button
           variant="contained"
           color="primary"
-          style={{ color: "red", border: "1px solid red", marginRight: "10px" }}
-          onClick={() => setactiveModal("")}
+          style={{ color: 'red', border: '1px solid red', marginRight: '10px' }}
+          onClick={() => setactiveModal('')}
         >
           Delete points
         </Button>
         <Button
           variant="contained"
           color="primary"
-          onClick={() => setactiveModal("")}
+          onClick={() => setactiveModal('')}
         >
           Continue scoring
         </Button>
@@ -238,9 +273,9 @@ export default function BeforePoints(props) {
   );
 
   const confirm = (
-    <div style={{ textAlign: "left" }}>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div style={{ marginRight: "59px" }}>
+    <div style={{ textAlign: 'left' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ marginRight: '59px' }}>
           <p>
             Name: NAME
             <br />
@@ -251,8 +286,8 @@ export default function BeforePoints(props) {
           </p>
         </div>
         <div>
-          <h3 style={{ margin: "0" }}>Realized Time</h3>
-          <h1 style={{ margin: "0" }}>00:00:00</h1>
+          <h3 style={{ margin: '0' }}>Realized Time</h3>
+          <h1 style={{ margin: '0' }}>00:00.000</h1>
         </div>
       </div>
 
@@ -265,7 +300,7 @@ export default function BeforePoints(props) {
                     penaltiesConf.find(
                       (e) => e.id === parseInt(penalty.penalty_conf_id)
                     ).name
-                  }{" "}
+                  }{' '}
                   --- {penalty.quantity}
                 </p>
               );
@@ -275,18 +310,18 @@ export default function BeforePoints(props) {
 
       <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginTop: "20px",
+          display: 'flex',
+          justifyContent: 'space-between',
+          marginTop: '20px',
         }}
       >
         <Button
-          style={{ color: "red", border: "1px solid red", marginRight: "10px" }}
-          onClick={() => setactiveModal("")}
+          style={{ color: 'red', border: '1px solid red', marginRight: '10px' }}
+          onClick={() => setactiveModal('')}
         >
           Cancel
         </Button>
-        <Button onClick={() => setactiveModal("")}>Continue scoring</Button>
+        <Button onClick={() => setactiveModal('')}>Continue scoring</Button>
       </div>
     </div>
   );
@@ -329,9 +364,10 @@ export default function BeforePoints(props) {
   //   </div>
   // );
 
-  const setTimer = (time) => {
-    setTime(tempTime);
-    setactiveModal("");
+  const setTimer = () => {
+    setbaseTime(tempTime);
+    // setpoint({ ...point, time: tempTime.replace(':', '').replace('.', '') });
+    setactiveModal('');
   };
 
   const customTempDefine = (
@@ -341,29 +377,29 @@ export default function BeforePoints(props) {
       </Typography>
       <TimeInput
         placeholder="00:00.000"
+        value={baseTime || undefined}
         inputType="time"
         onChange={(e) => setTempTime(e.target.value)}
       />
 
       <div
         style={{
-          display: "flex",
-          marginTop: "20px",
-          justifyContent: "space-between",
+          display: 'flex',
+          marginTop: '20px',
+          justifyContent: 'space-between',
         }}
       >
         <Button
           variant="outlined"
           color="secondary"
-          onClick={() => setactiveModal("")}
+          onClick={() => {
+            setactiveModal('');
+            setTempTime(0);
+          }}
         >
           Cancel
         </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => setTimer("")}
-        >
+        <Button variant="contained" color="primary" onClick={() => setTimer()}>
           Save Time
         </Button>
       </div>
@@ -382,14 +418,14 @@ export default function BeforePoints(props) {
   return (
     <>
       <Sidebar topnav title="Manageable Events" rightIcon="gear" />
-      <Card style={{ minHeight: "100%" }}>
-        <MainDiv style={{ marginTop: "50px", minHeight: "100%" }}>
+      <Card style={{ minHeight: '100%' }}>
+        <MainDiv style={{ marginTop: '50px', minHeight: '100%' }}>
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-between",
-              backgroundColor: "#6202EE",
-              padding: "14px 16px",
+              display: 'flex',
+              justifyContent: 'space-between',
+              backgroundColor: '#6202EE',
+              padding: '14px 16px',
             }}
           >
             <div>
@@ -425,11 +461,15 @@ export default function BeforePoints(props) {
             </div>
             <div
               style={{
-                display: "flex",
-                alignItems: "flex-end",
+                display: 'flex',
+                alignItems: 'flex-end',
               }}
             >
-              <Button variant="contained" color="primary">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => updateFinalTime()}
+              >
                 FINISH
               </Button>
             </div>
@@ -438,46 +478,46 @@ export default function BeforePoints(props) {
           <TimeDiv>
             <div
               style={{
-                width: "100%",
-                cursor: "pointer",
-                border: "1px solid",
-                borderRadius: "4px",
+                width: '100%',
+                cursor: 'pointer',
+                border: '1px solid',
+                borderRadius: '4px',
               }}
-              onClick={() => setactiveModal("customTempDefine")}
+              onClick={() => setactiveModal('customTempDefine')}
             >
               <Typography gutterBottom variant="h5" component="h2">
                 Base Time
               </Typography>
               <ShowTimeInput
                 readOnly
-                style={{ cursor: "pointer" }}
+                style={{ cursor: 'pointer' }}
                 placeholder="00:00.000"
                 inputType="time"
-                value={{ time }}
+                value={baseTime}
               />
             </div>
 
-            <div style={{ width: "100%", cursor: "context-menu" }}>
+            <div style={{ width: '100%', cursor: 'context-menu' }}>
               <Typography gutterBottom variant="h5" component="h2">
                 Total Time
               </Typography>
               <Typography variant="h5" component="h2">
-                00:00.000
+                {finalTime || '00:00.000'}
               </Typography>
             </div>
           </TimeDiv>
 
           <PenaltyDiv>
             {penaltiesConf.map((p, i) => {
-              return penalty(p);
+              return penalty(p, i);
             })}
           </PenaltyDiv>
         </MainDiv>
       </Card>
       <Modal
-        bodyStyle={{ padding: "25px 14px" }}
-        show={activeModal !== ""}
-        onBackgroundClick={() => setactiveModal("")}
+        bodyStyle={{ padding: '25px 14px' }}
+        show={activeModal !== ''}
+        onBackgroundClick={() => setactiveModal('')}
       >
         {modalContent(activeModal)}
       </Modal>
