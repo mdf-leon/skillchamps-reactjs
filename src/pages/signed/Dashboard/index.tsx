bbf0ab4ed57b0bbb0623a57bb446d1db9c9ee037
LuizGuilherme158 <55997260+LuizGuilherme158@users.noreply.github.com>
2020-11-16 21:41:05 -0300

    finalizado dash

--- src/pages/signed/Dashboard/index.tsx
+++ src/pages/signed/Dashboard/index.tsx
@@ -52,7 +52,7 @@ const useStyles = makeStyles({
 export default function CustomizedTables() {
   const classes = useStyles();
 
-  const [data, setData] = useState<any[]>([]);
+  const [data, setData] = useState<any>({});
   const [penaltyConfs, setPenaltyConfs] = useState<any>([]);
   const [bonusesConfs, setBonusesConfs] = useState<any>([]);
 
@@ -63,11 +63,14 @@ export default function CustomizedTables() {
       trial_id,
     };
     base
-      .get(`/fullRanking/${event_id}`)
+      .get(`/fullRanking2`, { params })
       .then((r) => {
-        setData(r.data.event.riders);
+        setData(r.data);
+        console.log(r.data);
       })
-      .catch((err) => {});
+      .catch((err) => {
+        console.log(err);
+      });
     base
       .get(`/managedPenaltyConfsFromTrial`, { params })
       .then((r) => {
@@ -84,7 +87,9 @@ export default function CustomizedTables() {
 
   return (
     <Card className={classes.root}>
-      <button onClick={() => console.log(data)}>aa</button>
+      <button onClick={() => console.log(data.riders[0].scores.penalties)}>
+        aa
+      </button>
       <CardContent>
         <TableContainer component={Paper}>
           <Table className={classes.table} aria-label="customized table">
@@ -92,59 +97,66 @@ export default function CustomizedTables() {
               <TableRow>
                 <StyledTableCell>DRIVER</StyledTableCell>
                 <StyledTableCell align="center">TIME</StyledTableCell>
-                {data[0]?.scores[0].penalties.map((pen) => {
-                  return (
-                    <StyledTableCell align="center">
-                      {
-                        penaltyConfs?.filter(
-                          (conf) => conf.id == pen?.penalty_conf_id
-                        )[0]?.name
-                      }
-                    </StyledTableCell>
-                  );
-                })}
-                {data[0]?.scores[1].bonuses.map((bonus) => {
-                  return (
-                    <StyledTableCell align="center">
-                      {
-                        bonusesConfs?.filter(
-                          (conf) => conf.id == bonus?.bonus_conf_id
-                        )[0]?.name
-                      }
-                    </StyledTableCell>
-                  );
-                })}
+                {data.riders && data.riders[0]
+                  ? data.riders[0].scores.penalties.map((pen) => {
+                      return (
+                        <StyledTableCell align="center">
+                          {
+                            penaltyConfs?.filter(
+                              (conf) => conf.id == pen?.penalty_conf_id
+                            )[0]?.name
+                          }
+                        </StyledTableCell>
+                      );
+                    })
+                  : null}
+                {data.riders && data.riders[0]
+                  ? data.riders[0].scores.bonuses?.map((bonus) => {
+                      return (
+                        <StyledTableCell align="center">
+                          {
+                            bonusesConfs?.filter(
+                              (conf) => conf.id == bonus?.bonus_conf_id
+                            )[0]?.name
+                          }
+                        </StyledTableCell>
+                      );
+                    })
+                  : null}
                 <StyledTableCell align="center">TOTAL TEMP</StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {data.map((row) => (
-                <StyledTableRow key={row.name}>
-                  <StyledTableCell component="th" scope="row">
-                    {row.name}
-                  </StyledTableCell>
-                  <StyledTableCell align="center">
-                    {row.scores[0].time}
-                  </StyledTableCell>
-                  {data[0]?.scores[0].penalties.map((pen) => {
-                    return (
-                      <StyledTableCell align="center">
-                        {pen.quantity}
-                      </StyledTableCell>
-                    );
-                  })}
-                  {data[0]?.scores[1].bonuses.map((bonus) => {
-                    return (
-                      <StyledTableCell align="center">
-                        {bonus.quantity}
-                      </StyledTableCell>
-                    );
-                  })}
-                  <StyledTableCell align="center">
-                    {row.scores[0].time_total}
-                  </StyledTableCell>
-                </StyledTableRow>
-              ))}
+              {data?.riders?.map((row) => {
+                if (!row.scores) return null;
+                return (
+                  <StyledTableRow key={row.name}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {row.scores?.time || 0}
+                    </StyledTableCell>
+                    {row.scores?.penalties?.map((pen) => {
+                      return (
+                        <StyledTableCell align="center">
+                          {pen.quantity}
+                        </StyledTableCell>
+                      );
+                    })}
+                    {row.scores?.bonuses?.map((bonus) => {
+                      return (
+                        <StyledTableCell align="center">
+                          {bonus.quantity}
+                        </StyledTableCell>
+                      );
+                    })}
+                    <StyledTableCell align="center">
+                      {row.scores?.time_total || 0}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                );
+              })}
             </TableBody>
           </Table>
         </TableContainer>
