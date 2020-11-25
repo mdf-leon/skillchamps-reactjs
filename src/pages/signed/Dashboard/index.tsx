d4e391eda91283c650fc3b93d6ae634db9409c22
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2020-11-24 21:43:28 -0300

    MINIMAL TABLE

--- src/pages/signed/Dashboard/index.tsx
+++ src/pages/signed/Dashboard/index.tsx
@@ -1,136 +1,61 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
-// import clsx from 'clsx';
-// import { lighten, makeStyles } from '@material-ui/core/styles';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-// import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-// import IconButton from '@material-ui/core/IconButton';
-// import Tooltip from '@material-ui/core/Tooltip';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Switch from '@material-ui/core/Switch';
-// import DeleteIcon from '@material-ui/icons/Delete';
-// import FilterListIcon from '@material-ui/icons/FilterList';
+import React, { useState, useEffect } from 'react';
+import AppBar from '../../../components/AppBar';
+import {
+  withStyles,
+  Theme,
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+import { useParams } from 'react-router-dom';
+import {
+  Card,
+  CardContent,
+  Paper,
+  TableRow,
+  TableHead,
+  TableContainer,
+  TableCell,
+  Typography,
+  TableBody,
+  Table,
+  TableSortLabel,
+} from '@material-ui/core';
+import { base } from '../../../config/api';
+import { Duration } from 'luxon';
 
-function createData(name, calories) {
-  return { name, calories };
-}
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
 
-const rows = [
-  createData('Cupcake', 305),
-  createData('Donut', 452),
-  createData('Eclair', 262),
-  createData('Nougat', 360),
-  createData('Oreo', 437),
-];
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  {
-    id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Dessert (100g serving)',
-  },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-];
-
-function EnhancedTableHead(props) {
-  const {
-    classes,
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ 'aria-label': 'select all desserts' }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'default'}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  })
+)(TableRow);
 
 const useStyles = makeStyles((theme) => ({
+  table: {},
   root: {
+    minWidth: 275,
     width: '100%',
   },
   paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   visuallyHidden: {
     border: 0,
@@ -147,157 +72,182 @@ const useStyles = makeStyles((theme) => ({
 
 type Order = 'asc' | 'desc';
 
-export default function EnhancedTable() {
+export default function CustomizedTables(props: any) {
   const classes = useStyles();
+
+  const [data, setData] = useState<any>({});
+  const [penaltyConfs, setPenaltyConfs] = useState<any>([]);
+  const [bonusesConfs, setBonusesConfs] = useState<any>([]);
+
   const [order, setOrder] = React.useState<Order>('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
-  const [selected, setSelected] = React.useState<any[]>([]);
+  const [orderBy, setOrderBy] = React.useState('position');
+
+  let { trial_id, event_id } = useParams();
+  useEffect(() => {
+    let params: any = {
+      event_id,
+      trial_id,
+    };
+    base
+      .get(`/fullRanking2`, { params })
+      .then((r) => {
+        setData(r.data);
+      })
+      .catch(() => {});
+    base
+      .get(`/managedPenaltyConfsFromTrial`, { params })
+      .then((r) => {
+        setPenaltyConfs(r.data);
+      })
+      .catch(() => {});
+    base
+      .get(`/managedBonusConfsFromTrial`, { params })
+      .then((r) => {
+        setBonusesConfs(r.data);
+      })
+      .catch(() => {});
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const msToDefault = (ms) => {
+    const duration = Duration.fromObject({ milliseconds: ms })
+      .normalize()
+      .shiftTo('minutes', 'seconds', 'milliseconds')
+      .toObject();
+    const minutesT = `${duration.minutes}`.padStart(2, '0');
+    const secondsT = `${duration.seconds}`.padStart(2, '0');
+    const millisecondsT = `${duration.milliseconds}`.padEnd(3, '0');
+    const timeT = `${minutesT}:${secondsT}.${millisecondsT}`;
+    return timeT;
+  };
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
   };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: any[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
-  };
-
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
   const createSortHandler = (property) => (event) => {
     handleRequestSort(event, property);
+    console.log("oi");
+    
   };
 
   return (
-    <div className={classes.root}>
-      <TableContainer>
-        <Table
-          className={classes.table}
-          aria-labelledby="tableTitle"
-          aria-label="enhanced table"
-        >
-          {/* <EnhancedTableHead
-            classes={classes}
-            numSelected={selected.length}
-            order={order}
-            orderBy={orderBy}
-            onSelectAllClick={handleSelectAllClick}
-            onRequestSort={handleRequestSort}
-            rowCount={rows.length}
-          /> */}
-          <TableHead>
-            <TableRow>
-              <TableCell
-                key={'name'}
-                align={false ? 'right' : 'left'}
-                padding={false ? 'none' : 'default'}
-                sortDirection={orderBy === 'name' ? order : false}
-              >
-                <TableSortLabel
-                  active={orderBy === 'name'}
-                  direction={orderBy === 'name' ? order : 'asc'}
-                  onClick={createSortHandler('name')}
-                >
-                  name
-                  {orderBy === 'name' ? (
-                    <span className={classes.visuallyHidden}>
-                      {order === 'desc'
-                        ? 'sorted descending'
-                        : 'sorted ascending'}
-                    </span>
-                  ) : null}
-                </TableSortLabel>
-              </TableCell>
-              <TableCell
-                key={'calories'}
-                align={false ? 'right' : 'left'}
-                padding={false ? 'none' : 'default'}
-                sortDirection={orderBy === 'calories' ? order : false}
-              >
-                <TableSortLabel
-                  active={orderBy === 'calories'}
-                  direction={orderBy === 'calories' ? order : 'asc'}
-                  onClick={createSortHandler('calories')}
-                >
-                  calories
-                  {orderBy === 'calories' ? (
-                    <span className={classes.visuallyHidden}>
-                      {order === 'desc'
-                        ? 'sorted descending'
-                        : 'sorted ascending'}
-                    </span>
-                  ) : null}
-                </TableSortLabel>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {stableSort(rows, getComparator(order, orderBy)).map(
-              (row, index) => {
-                const isItemSelected = isSelected(row.name);
-                const labelId = `enhanced-table-checkbox-${index}`;
-
-                return (
-                  <TableRow
-                    hover
-                    onClick={(event) => handleClick(event, row.name)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.name}
-                    selected={isItemSelected}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={isItemSelected}
-                        inputProps={{ 'aria-labelledby': labelId }}
-                      />
-                    </TableCell>
-                    <TableCell
-                      component="th"
-                      id={labelId}
-                      scope="row"
-                      padding="none"
+    <>
+      <AppBar title={props.location.state.trialName} {...props} />
+      {data?.riders && data.riders[0].scores ? (
+        <Card className={classes.root}>
+          <CardContent>
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell
+                      key="position"
+                      sortDirection={orderBy === 'position' ? order : false}
                     >
-                      {row.name}
-                    </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                      <TableSortLabel
+                        active={orderBy === 'position'}
+                        direction={orderBy === 'position' ? order : 'asc'}
+                        onClick={createSortHandler('position')}
+                      >
+                        POSITION
+                        {orderBy === 'position' ? (
+                          <span className={classes.visuallyHidden}>
+                            {order === 'desc'
+                              ? 'sorted descending'
+                              : 'sorted ascending'}
+                          </span>
+                        ) : null}
+                      </TableSortLabel>
+                    </StyledTableCell>
+                    <StyledTableCell>DRIVER</StyledTableCell>
+                    <StyledTableCell align="center">TIME</StyledTableCell>
+                    {data.riders && data.riders[0]
+                      ? data.riders[0].scores.penalties.map((pen) => {
+                          return (
+                            <StyledTableCell key={"pen-conf-id-"+pen.penalty_conf_id} align="center">
+                              {
+                                penaltyConfs?.filter(
+                                  (conf) => conf.id === pen?.penalty_conf_id
+                                )[0]?.name
+                              }
+                            </StyledTableCell>
+                          );
+                        })
+                      : null}
+                    <StyledTableCell align="center">
+                      PENALTIES TOTAL
+                    </StyledTableCell>
+                    {data.riders && data.riders[0]
+                      ? data.riders[0].scores.bonuses?.map((bonus) => {
+                          return (
+                            <StyledTableCell key={"bon-conf-id-"+bonus?.bonus_conf_id} align="center">
+                              {
+                                bonusesConfs?.filter(
+                                  (conf) => conf.id === bonus?.bonus_conf_id
+                                )[0]?.name
+                              }
+                            </StyledTableCell>
+                          );
+                        })
+                      : null}
+                    <StyledTableCell align="center">
+                      BONUSES TOTAL
+                    </StyledTableCell>
+                    <StyledTableCell align="center">TOTAL TEMP</StyledTableCell>
                   </TableRow>
-                );
-              }
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+                </TableHead>
+                <TableBody>
+                  {data?.riders?.map((row, i) => {
+                    if (!row.scores) return null;
+                    return (
+                      <StyledTableRow key={row.name}>
+                        <StyledTableCell component="th" scope="row">
+                          {i + 1}
+                        </StyledTableCell>
+                        <StyledTableCell component="th" scope="row">
+                          {row.name}
+                        </StyledTableCell>
+                        <StyledTableCell align="center">
+                          {row.scores?.time ? msToDefault(row.scores?.time) : 0}
+                        </StyledTableCell>
+                        {row.scores?.penalties?.map((pen) => {
+                          return (
+                            <StyledTableCell key={pen.id+"-pen-quantity"} align="center">
+                              {pen.quantity}
+                            </StyledTableCell>
+                          );
+                        })}
+                        <StyledTableCell align="center">n/a</StyledTableCell>
+                        {row.scores?.bonuses?.map((bonus) => {
+                          return (
+                            <StyledTableCell key={bonus.id+"-bon-quantity"} align="center">
+                              {bonus.quantity}
+                            </StyledTableCell>
+                          );
+                        })}
+                        <StyledTableCell align="center">n/a</StyledTableCell>
+                        <StyledTableCell align="center">
+                          {row.scores?.time_total
+                            ? msToDefault(row.scores?.time_total)
+                            : 0}
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
+        </Card>
+      ) : (
+        <div style={{ paddingTop: '60px', textAlign: 'center' }}>
+          <Typography component="h1" variant="h5">
+            You don't have any scores yet
+          </Typography>
+        </div>
+      )}
+    </>
   );
 }
