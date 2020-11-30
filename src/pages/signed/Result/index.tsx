0cd26af5d3095f883ab511e23fc2b588af35a4e7
LuizGuilherme158 <55997260+LuizGuilherme158@users.noreply.github.com>
2020-11-30 00:54:39 -0300

    tela de results feita

--- src/pages/signed/Result/index.tsx
+++ src/pages/signed/Result/index.tsx
@@ -1,16 +1,87 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardContent } from "@material-ui/core";
 import AppBar from "../../../components/AppBar";
-import { base } from "../../../config/api";
-import FirstTable from "./tables/firstTable";
+import {
+  withStyles,
+  Theme,
+  createStyles,
+  makeStyles,
+} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import {
+  Paper,
+  TableRow,
+  TableHead,
+  TableContainer,
+  TableBody,
+  Table,
+  Typography,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import qs from "query-string";
+import { base } from "config/api";
+import { TableCell, TitleDiv, TheConeMasterDiv } from "./styles";
+// import { Duration } from "luxon";
+
+//TableSortLabel
+// const StyledTableSortLabel = withStyles((theme: Theme) =>
+//   createStyles({
+//     head: {
+//       backgroundColor: theme.palette.common.black,
+//       color: theme.palette.common.white,
+//     },
+//     body: {
+//       fontSize: 14,
+//     },
+//   })
+// )(TableSortLabel);
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {},
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  })
+)(TableRow);
 
 const useStyles = makeStyles((theme) => ({
+  table: {},
+  paper: {
+    borderTopLeftRadius: "0px",
+    borderTopRightRadius: "0px",
+  },
   root: {
     minWidth: 275,
     width: "100%",
+    height: "calc(100% - 64px)",
+  },
+  // paper: {
+  //   marginTop: theme.spacing(8),
+  //   display: "flex",
+  //   flexDirection: "column",
+  //   alignItems: "center",
+  // },
+  visuallyHidden: {
+    border: 0,
+    clip: "rect(0 0 0 0)",
+    height: 1,
+    margin: -1,
+    overflow: "hidden",
+    padding: 0,
+    position: "absolute",
+    top: 20,
+    width: 1,
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
@@ -19,9 +90,23 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
     minWidth: 120,
   },
+  gridConeMaster: {
+    display: "flex",
+    justifyContent: "center",
+  },
 }));
 
-export default function CustomizedTables(props: any) {
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+export default function FirstTable(props: any) {
   const classes = useStyles();
   const [data, setData] = useState<any>({});
 
@@ -53,11 +138,132 @@ export default function CustomizedTables(props: any) {
       .catch(() => {});
   }, []);
 
+  const customTableConeMaster = (coner_mater) => {
+    return (
+      <>
+        <TitleDiv>
+          <Typography component="h5" variant="subtitle1">
+            Tie: THE CONE MASTER
+          </Typography>
+        </TitleDiv>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="customized table">
+            <TableBody>
+              {coner_mater?.map((row, i) => {
+                return (
+                  <StyledTableRow key={i + 1}>
+                    <StyledTableCell align="center">
+                      <div
+                        style={{ display: "flex", justifyContent: "center" }}
+                      >
+                        <Typography
+                          component="p"
+                          variant="subtitle2"
+                          color="textSecondary"
+                        >
+                          {row.id}.&ensp;
+                        </Typography>
+                        {row.name}
+                      </div>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
+  };
+
+  const customTable = (event) => {
+    return (
+      <>
+        <TitleDiv>
+          <Typography component="h5" variant="subtitle1">
+            {event.trial_name}{" "}
+            {event.category_chosen !== "null" &&
+            event.category_chosen !== "none"
+              ? event.category_chosen
+              : null}
+          </Typography>
+        </TitleDiv>
+        <TableContainer component={Paper} className={classes.paper}>
+          <Table className={classes.table} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align="center">CLASSIFICATION</StyledTableCell>
+                <StyledTableCell align="center">NAME</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {event?.riders?.map((row, i) => {
+                if (i <= 2) {
+                  return (
+                    <StyledTableRow key={i + 1}>
+                      <StyledTableCell align="center">{i + 1}</StyledTableCell>
+                      <StyledTableCell align="center">
+                        {row.name ? (
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "center",
+                            }}
+                          >
+                            <Typography
+                              component="p"
+                              variant="subtitle2"
+                              color="textSecondary"
+                            >
+                              {row.id}.&ensp;
+                            </Typography>
+                            {row.name}
+                          </div>
+                        ) : null}
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  );
+                }
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </>
+    );
+  };
+
   return (
     <>
       <AppBar title="Result" {...props} />
       <Card className={classes.root}>
-        <CardContent>{/* <FirstTable data={data} /> */}</CardContent>
+        <CardContent>
+          <Grid container spacing={3}>
+            <Grid className={classes.gridConeMaster} item xs={12}>
+              <Grid item xs={4}>
+                {data?.the_cone_master?.length === 1 ? (
+                  <TheConeMasterDiv>
+                    <Typography component="h5" variant="subtitle1">
+                      THE CONE MASTER
+                    </Typography>
+                    <div>
+                      <Typography component="h4" variant="h4">
+                        {data?.the_cone_master[0]?.name}
+                      </Typography>
+                    </div>
+                  </TheConeMasterDiv>
+                ) : (
+                  customTableConeMaster(data?.the_cone_master)
+                )}
+              </Grid>
+            </Grid>
+
+            {data?.total_events?.map((event, i) => (
+              <Grid item key={i} xs={4}>
+                {customTable(event)}
+              </Grid>
+            ))}
+          </Grid>
+        </CardContent>
       </Card>
     </>
   );
--- src/pages/signed/Result/styles.ts
+++ src/pages/signed/Result/styles.ts
@@ -1,6 +1,32 @@
 import styled from 'styled-components'
 import { TableCell as TableCellComponent, TableSortLabel as TableSortLabelComponent } from "@material-ui/core";
 
+export const TitleDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
+  padding-left: 5px;
+  border-bottom: 1px solid rgba(224, 224, 224, 1);
+`;
+
+export const TheConeMasterDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
+  text-align: center;
+  justify-content: center;
+  border-radius: 4px;
+  h5 {
+    width: 100%;
+    border-bottom: 1px solid rgba(224, 224, 224, 1);
+  }
+  div{
+    padding: 20px 0px;
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+`;
 
 export const TableCell = styled(TableCellComponent)`
   overflow: hidden;
