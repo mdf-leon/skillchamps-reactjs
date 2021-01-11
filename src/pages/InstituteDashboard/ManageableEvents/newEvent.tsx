1aaaf0a85df115a785ce700f77a5441213b4e91d
LuizGuilherme158 <55997260+LuizGuilherme158@users.noreply.github.com>
2021-01-10 21:44:55 -0300

    commit

--- src/pages/InstituteDashboard/ManageableEvents/newEvent.tsx
+++ src/pages/InstituteDashboard/ManageableEvents/newEvent.tsx
@@ -1,48 +1,49 @@
-import React, { useState } from 'react';
-import Message from 'components/Message';
-import AppBar from '../../../components/AppBar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import 'date-fns';
-import DateFnsUtils from '@date-io/date-fns';
+import React, { useState } from "react";
+import Message from "components/Message";
+import AppBar from "../../../components/AppBar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import "date-fns";
+import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
-} from '@material-ui/pickers';
-import { base } from 'config/api';
+} from "@material-ui/pickers";
+import { base } from "config/api";
+import { UploadInputDiv, UploadInputLabel } from "./styles";
 // import { FaceForm } from './Image';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
   root: {
-    '& .MuiTextField-root': {
+    "& .MuiTextField-root": {
       margin: theme.spacing(1),
-      width: '25ch',
+      width: "25ch",
     },
   },
   date: {
-    width: '100%',
-    margin: '8px 9px',
+    width: "100%",
+    margin: "8px 9px",
   },
   category: {
-    width: '100%',
+    width: "100%",
   },
 }));
 
@@ -51,12 +52,12 @@ export default function NewRider(props: any) {
   const [selectedDate, setSelectedDate] = useState<any>(new Date());
   const [eventPhoto, seteventPhoto] = useState<any>(undefined);
   const [messageParams, setMessageParams] = useState<any>({
-    message: '',
-    severity: '',
+    message: "",
+    severity: "",
   });
   const [registerInfo, setRegisterInfo] = useState<any>({
-    event_name: '',
-    date_begin: selectedDate?.toISOString().split('T')[0],
+    event_name: "",
+    date_begin: selectedDate?.toISOString().split("T")[0],
   });
 
   const handleSubmit = async (e) => {
@@ -64,7 +65,7 @@ export default function NewRider(props: any) {
     console.log(eventPhoto);
     const body = {
       ...registerInfo,
-      date_begin: selectedDate?.toISOString().split('T')[0],
+      date_begin: selectedDate?.toISOString().split("T")[0],
     };
     const event = await base
       .post(`/createEvent`, body)
@@ -73,36 +74,36 @@ export default function NewRider(props: any) {
       })
       .catch(() => {
         setMessageParams({
-          message: 'Sorry, the Institute could not be created',
-          severity: 'error',
+          message: "Sorry, the Institute could not be created",
+          severity: "error",
         });
         return false;
       }); // alert rider coundt be created
-      console.log(event.id);
+    console.log(event.id);
     if (!event || !event.id) return false;
 
     const formData = new FormData();
-    formData.append('photo_event', eventPhoto);
+    formData.append("photo_event", eventPhoto);
     await base
       .put(`/uploadEventPhoto/${event.id}`, formData, {
         headers: {
-          'content-type': 'multipart/form-data',
+          "content-type": "multipart/form-data",
         },
       })
       .then((r) => {
         console.log(r.data);
-        props.history.push('/ManageableEvents', {
+        props.history.push("/ManageableEvents", {
           message_alert: {
-            message: 'Event created Successfully',
-            severity: 'success',
+            message: "Event created Successfully",
+            severity: "success",
           },
         });
       })
       .catch((e) => {
-        console.log(e)
+        console.log(e);
         setMessageParams({
-          message: 'Sorry, the Image was not uploaded',
-          severity: 'error',
+          message: "Sorry, the Image was not uploaded",
+          severity: "error",
         });
         return false;
       });
@@ -125,7 +126,7 @@ export default function NewRider(props: any) {
         trial success
       </Button> */}
       <AppBar title="Create a New Event" {...props} />
-      <div style={{ paddingTop: '1px', minHeight: '100%' }}>
+      <div style={{ paddingTop: "1px", minHeight: "100%" }}>
         <Container component="main" maxWidth="xs">
           <CssBaseline />
           <div className={classes.paper}>
@@ -142,6 +143,7 @@ export default function NewRider(props: any) {
                         event_name: e.target.value,
                       })
                     }
+                    type="file"
                     name="event_name"
                     variant="outlined"
                     required
@@ -163,11 +165,47 @@ export default function NewRider(props: any) {
                       value={selectedDate}
                       onChange={setSelectedDate}
                       KeyboardButtonProps={{
-                        'aria-label': 'change date',
+                        "aria-label": "change date",
                       }}
                     />
                   </Grid>
                 </MuiPickersUtilsProvider>
+                <Grid item xs={12}>
+                  <div style={{ position: "relative", width: "100%" }}>
+                    <TextField
+                      name="photo_eventt"
+                      id="photo_eventt"
+                      variant="outlined"
+                      required
+                      fullWidth
+                      InputLabelProps={{ shrink: true }}
+                      label="Select a photo_event"
+                    />
+                    <UploadInputDiv>
+                      <Button
+                        variant="contained"
+                        component="label"
+                        color="primary"
+                      >
+                        Upload File
+                        <input
+                          type="file"
+                          id="photo_event"
+                          name="photo_event"
+                          hidden
+                          // value={eventPhoto}
+                          onChange={(e) => {
+                            if (e && e.target && e.target.files)
+                              seteventPhoto(e.target.files[0]);
+                          }}
+                        />
+                      </Button>
+                      <UploadInputLabel htmlFor="photo_event">
+                        Select file
+                      </UploadInputLabel>
+                    </UploadInputDiv>
+                  </div>
+                </Grid>
               </Grid>
               {/* <Grid container justify="space-around">
                 <label htmlFor="photo_folder">Select a photo_folder:</label>
@@ -182,19 +220,10 @@ export default function NewRider(props: any) {
                   }}
                 />
               </Grid> */}
-              <Grid container justify="space-around">
-                <label htmlFor="photo_event">Select a photo_event:</label>
-                <input
-                  type="file"
-                  id="photo_event"
-                  name="photo_event"
-                  // value={eventPhoto}
-                  onChange={(e) => {
-                    if (e && e.target && e.target.files)
-                      seteventPhoto(e.target.files[0]);
-                  }}
-                />
-              </Grid>
+              {/* <Grid container justify="space-around"> */}
+              {/* <label htmlFor="photo_event">Select a photo_event:</label> */}
+
+              {/* </Grid> */}
               <Button
                 type="submit"
                 fullWidth
---
+++ src/pages/InstituteDashboard/ManageableEvents/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const UploadInputDiv = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  left: 14px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const UploadInputLabel = styled.label`
+  font-size: 16px;
+  margin-left: 6px;
+`;
