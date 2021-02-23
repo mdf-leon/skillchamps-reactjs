8bbdc1b672b4403f2406c385b6cf570ef719df96
LuizGuilherme158 <55997260+LuizGuilherme158@users.noreply.github.com>
2021-02-23 12:08:55 -0300

    Add manager feito, pelo que vi era só um input com botão add manager e texto apago

--- src/pages/InstituteDashboard/AddManager/index.tsx
+++ src/pages/InstituteDashboard/AddManager/index.tsx
@@ -144,230 +144,41 @@ export default function NewTrials(props: any) {
 
   return (
     <>
-      <Message
-        message={messageParams.message}
-        severity={messageParams.severity}
-        {...props}
-      />
-      <AppBar title="Choose new trial" isManager {...props} />
-      <div style={{ paddingTop: "1px", minHeight: "100%" }}>
+      <AppBar title="Add Manager" isManager {...props} />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          paddingTop: "1px",
+          paddingBottom: "64px",
+          minHeight: "100%",
+        }}
+      >
         <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <div className={classes.paper}>
-            <Typography component="h1" variant="h5">
-              Create new Trial
-            </Typography>
-            <form onSubmit={handleSubmit} className={classes.form} noValidate>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    onChange={(e) =>
-                      setRegisterInfo({ ...registerInfo, name: e.target.value })
-                    }
-                    autoComplete="name"
-                    name="Name"
-                    variant="outlined"
-                    fullWidth
-                    id="Name"
-                    label="Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormControl
-                    fullWidth
-                    variant="outlined"
-                    className={classes.formControl}
-                  >
-                    <InputLabel id="demo-simple-select-outlined-label">
-                      inverted time? (first place is bigger time)
-                    </InputLabel>
-                    <Select
-                      name="inverted"
-                      label="inverted time? (first place is bigger time)"
-                      labelId="inverted_time"
-                      id="inverted"
-                      value={registerInfo.inverted}
-                      onChange={(e) =>
-                        setRegisterInfo({
-                          ...registerInfo,
-                          inverted: e.target.value,
-                        })
-                      }
-                    >
-                      <MenuItem value="false">
-                        <em>No</em>
-                      </MenuItem>
-                      <MenuItem value="true">Yes</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Grid>
-                <Typography
-                  style={{ textAlign: "center", width: "100%" }}
-                  component="h1"
-                  variant="h5"
-                >
-                <Grid item xs={12}>
-                  Penalties
-                </Typography>
-                <Grid item xs={12}>
-                  <TextField
-                    onChange={(e) =>
-                      setTempPenalties({
-                        ...tempPenalties,
-                        name: e.target.value,
-                      })
-                    }
-                    value={tempPenalties.name}
-                    variant="outlined"
-                    fullWidth
-                    name="penaltyName"
-                    label="Penalty name"
-                    id="Penalty name"
-                  />
-                </Grid>
-                    
-              <Button
-                type="button"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                onClick={() => handleAddBonuses()}
-              >
-                Add Bonuses
-              </Button>
-              {/* RENDERS */}
-              {dataPenalties[0] ? (
-                <Typography
-                  style={{ textAlign: "center", width: "100%" }}
-                  component="h1"
-                  variant="h5"
-                >
-                  Penalties
-                </Typography>
-              ) : null}
-              {dataPenalties.map((content, i) => (
-                <div
-                  key={`TrialList${content.name}`}
-                  className={classes.options}
-                >
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <Typography
-                      component={"span"}
-                      style={{ margin: 0 }}
-                      gutterBottom
-                      variant="h6"
-                      color="textSecondary"
-                    >
-                      {content.name}
-                    </Typography>
-                    <Typography
-                      component={"span"}
-                      style={{ margin: 0 }}
-                      gutterBottom
-                      variant="h6"
-                    >
-                      {content.description}
-                    </Typography>
-                    <Typography
-                      component={"span"}
-                      style={{ margin: 0 }}
-                      gutterBottom
-                      variant="body2"
-                      color="textSecondary"
-                    >
-                      {content.time_penalty}
-                    </Typography>
-                  </div>
-                  <Button
-                    onClick={() => {
-                      const temp = [...dataPenalties];
-                      // delete dataPenalties[i];
-                      temp.splice(i, 1);
-                      setdataPenalties(temp);
-                    }}
-                    className={classes.action}
-                    size="small"
-                    color="secondary"
-                  >
-                    Delete
-                  </Button>
-                </div>
-              ))}
-
-              {dataBonuses[0] ? (
-                <Typography
-                  style={{
-                    textAlign: "center",
-                    width: "100%",
-                    marginTop: "16px",
-                  }}
-                  component="h1"
-                  variant="h5"
-                >
-                  Bonuses
-                </Typography>
-              ) : null}
-              {dataBonuses.map((content, i) => (
-                <div
-                  key={`TrialList${content.name}`}
-                  className={classes.options}
-                >
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <Typography
-                      component={"span"}
-                      style={{ margin: 0 }}
-                      gutterBottom
-                      variant="h6"
-                      color="textSecondary"
-                    >
-                      {content.name}
-                    </Typography>
-                    <Typography
-                      component={"span"}
-                      style={{ margin: 0 }}
-                      gutterBottom
-                      variant="h6"
-                    >
-                      {content.description}
-                    </Typography>
-                    <Typography
-                      component={"span"}
-                      style={{ margin: 0 }}
-                      gutterBottom
-                      variant="body2"
-                      color="textSecondary"
-                    >
-                      {content.time_bonus}
-                    </Typography>
-                  </div>
-                  <Button
-                    onClick={() => {
-                      const temp = [...dataBonuses];
-                      // delete dataBonuses[i];
-                      temp.splice(i, 1);
-                      setdataBonuses(temp);
-                    }}
-                    className={classes.action}
-                    size="small"
-                    color="secondary"
-                  >
-                    Delete
-                  </Button>
-                </div>
-              ))}
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-              >
-                Create
-              </Button>
-            </form>
-          </div>
+          <Typography align="center" component="h1" variant="h5">
+            Add Manager
+          </Typography>
+          <TextField
+            className="mt-20"
+            onChange={(e) =>
+              setRegisterInfo({ ...registerInfo, name: e.target.value })
+            }
+            autoComplete="name"
+            name="Name"
+            variant="outlined"
+            fullWidth
+            id="Name"
+            label="Name"
+            autoFocus
+          />
+          <Button
+            className="mt-20"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Add Manager
+          </Button>
         </Container>
       </div>
     </>
