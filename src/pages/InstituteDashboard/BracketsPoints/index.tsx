5d2b0d3851b7b1e8e5949f0db22e4cf905825a29
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2021-04-10 23:11:40 -0300

    new points brackets

--- src/pages/InstituteDashboard/BracketsPoints/index.tsx
+++ src/pages/InstituteDashboard/BracketsPoints/index.tsx
@@ -1,53 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 // import Message from "components/Message";
-import { useParams, useLocation } from "react-router-dom";
-import AppBar from "components/AppBar";
-import { Modal } from "components";
+import { useParams, useLocation } from 'react-router-dom';
+import AppBar from 'components/AppBar';
+import { Modal } from 'components';
 import {
   Card,
   CardContent,
   CardActions,
   Button,
   Typography,
-} from "@material-ui/core";
-import { MainPage, Bracket, Line } from "./styles";
-import { base } from "../../../config/api";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
+} from '@material-ui/core';
+import { MainPage, Bracket, Line } from './styles';
+import { base } from '../../../config/api';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() =>
   createStyles({
     mainCardText: {
-      color: "white",
+      color: 'white',
     },
     numberText: {
       fontWeight: 300,
     },
     content: {
-      display: "flex",
-      justifyContent: "space-between",
-      paddingBottom: "0px",
+      display: 'flex',
+      justifyContent: 'space-between',
+      paddingBottom: '0px',
     },
     action: {
-      position: "unset",
+      position: 'unset',
     },
   })
 );
 
 export default function AddScore(props) {
   const classes = useStyles();
-  let trial_id = new URLSearchParams(useLocation().search);
-  const [activeModal, setactiveModal] = useState<any>("");
-  const { group_id, position_id } = useParams();
+  let qparams = new URLSearchParams(useLocation().search);
+  const [activeModal, setactiveModal] = useState<any>('');
+  const { institute_id, event_id, group_id, position_id } = useParams();
   const [bracketData, setBrecketData] = useState<any>();
 
-  const [riderSelected, setRiderSelected] = useState<any>();
+  const [riderSelected, setRiderSelected] = useState<any>(0);
   console.log(riderSelected);
 
-  const handleFinish = () => {};
+  const handleFinish = () => {
+    console.log({
+      winner: bracketData && bracketData[`rider${riderSelected}`]?.id,
+    });
+
+    base
+      .put(
+        `/bracketScore/${qparams.get('trial_id')}/${group_id}/${position_id}`,
+        {
+          winner: bracketData && bracketData[`rider${riderSelected}`]?.id,
+        }
+      )
+      .then((res) => {
+        // props.history.push(
+        //   `/dashboard/institute/${institute_id}/manage/event/${event_id}/score/select_trial_rider`
+        // );
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     base
-      .get(`/trial/${trial_id.get("trial_id")}/getBrackets`)
+      .get(`/trial/${qparams.get('trial_id')}/getBrackets`)
       .then((res) => {
         setBrecketData(res.data.tournament[group_id][position_id]);
         console.log(res.data.tournament[group_id][position_id]);
@@ -60,26 +79,28 @@ export default function AddScore(props) {
     <Card>
       <CardContent className={classes.content}>
         <div
-          style={{ display: "flex", flexDirection: "column", width: "100%" }}
+          style={{ display: 'flex', flexDirection: 'column', width: '100%' }}
         >
           <Typography
-            style={{ textAlign: "center" }}
+            style={{ textAlign: 'center' }}
             gutterBottom
             variant="h5"
             component="h2"
           >
-            Do you really want to finish scoring for the rider{""}
+            Do you really want to finish scoring for the rider
+            {bracketData && bracketData[`rider${riderSelected}`]?.name}
+            TODO CARDZINHO DO RIDER
             {/* {dataRider.name}? */}
           </Typography>
         </div>
       </CardContent>
-      <CardActions style={{ justifyContent: "center" }}>
+      <CardActions style={{ justifyContent: 'center' }}>
         <Button
           className={classes.action}
           variant="contained"
           size="small"
           color="primary"
-          onClick={() => setactiveModal("")}
+          onClick={() => setactiveModal('')}
         >
           Cancel
         </Button>
@@ -106,10 +127,10 @@ export default function AddScore(props) {
   return (
     <div>
       <Modal
-        bodyStyle={{ margin: "auto 20px", width: "100%" }}
+        bodyStyle={{ margin: 'auto 20px', width: '100%' }}
         noPadding
-        show={activeModal !== ""}
-        onBackgroundClick={() => setactiveModal("")}
+        show={activeModal !== ''}
+        onBackgroundClick={() => setactiveModal('')}
       >
         {modalContent(activeModal)}
       </Modal>
@@ -126,12 +147,12 @@ export default function AddScore(props) {
           </div>
           <Bracket
             isFocus={riderSelected === 0}
-            onClick={() => setRiderSelected(0)}
+            onClick={() => setRiderSelected(1)}
           >
             <div>
               <div className="d-flex">
                 <Typography
-                  component={"span"}
+                  component={'span'}
                   style={{ margin: 0 }}
                   gutterBottom
                   variant="h6"
@@ -140,12 +161,12 @@ export default function AddScore(props) {
                   {bracketData?.rider1.id}
                   .&nbsp;
                 </Typography>
-                <Typography component={"span"} gutterBottom variant="h6">
+                <Typography component={'span'} gutterBottom variant="h6">
                   {bracketData?.rider1.name}
                 </Typography>
               </div>
               <Typography
-                component={"span"}
+                component={'span'}
                 style={{ margin: 0 }}
                 gutterBottom
                 variant="body2"
@@ -160,12 +181,12 @@ export default function AddScore(props) {
 
           <Bracket
             isFocus={riderSelected === 1}
-            onClick={() => setRiderSelected(1)}
+            onClick={() => setRiderSelected(2)}
           >
             <div>
               <div className="d-flex">
                 <Typography
-                  component={"span"}
+                  component={'span'}
                   style={{ margin: 0 }}
                   gutterBottom
                   variant="h6"
@@ -174,12 +195,12 @@ export default function AddScore(props) {
                   {bracketData?.rider2.id}
                   .&nbsp;
                 </Typography>
-                <Typography component={"span"} gutterBottom variant="h6">
+                <Typography component={'span'} gutterBottom variant="h6">
                   {bracketData?.rider2.name}
                 </Typography>
               </div>
               <Typography
-                component={"span"}
+                component={'span'}
                 style={{ margin: 0 }}
                 gutterBottom
                 variant="body2"
@@ -189,19 +210,22 @@ export default function AddScore(props) {
               </Typography>
             </div>
           </Bracket>
-          <div className="mt-40" style={{ width: "100%" }}>
+          <div className="mt-40" style={{ width: '100%' }}>
             <p className="mb-4">Confirm the winner</p>
             <Button
               className={classes.action}
               variant="contained"
               color="secondary"
+              onClick={() =>
+                riderSelected !== 0
+                  ? setactiveModal('finishConfirm')
+                  : setactiveModal('')
+              }
               fullWidth
             >
-              {riderSelected === 0 || riderSelected === 1
-                ? `Confirm rider: ${
-                    bracketData[`rider${riderSelected + 1}`].name
-                  }`
-                : "Confirm"}
+              {riderSelected !== 0
+                ? `Confirm rider: ${bracketData[`rider${riderSelected}`].name}`
+                : 'Confirm'}
             </Button>
           </div>
         </div>
