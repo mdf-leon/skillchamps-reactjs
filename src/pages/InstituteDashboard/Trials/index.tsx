0e32aeb85ab5f0a85db3164fb32371fbaf9a3478
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2021-04-25 19:39:58 -0300

    minor fixes

--- src/pages/InstituteDashboard/Trials/index.tsx
+++ src/pages/InstituteDashboard/Trials/index.tsx
@@ -18,7 +18,7 @@ import { useParams } from 'react-router-dom';
 export default function Trials(props: any) {
   const classes = styles();
   const { institute_id, event_id } = useParams();
-  const [trialsList, settrialsList] = useState<any[]>([]); 
+  const [trialsList, settrialsList] = useState<any[]>([]);
   const [activeModal, setActiveModal] = useState<any>('');
   const [currentId, setCurrentId] = useState<any>('');
 
@@ -43,8 +43,8 @@ export default function Trials(props: any) {
             variant="body2"
             component="p"
           >
-            Do you really want to delete this Trial? This process cannot be
-            undone.
+            Deleting a trial means it will be deactivated. In order to revert this action you need to contact a system administrator at 
+            <a href="http://contact.skillchamps.net">http://contact.skillchamps.net</a>. This process can take a while, so proceed with caution.
           </Typography>
         </div>
       </CardContent>
@@ -70,10 +70,58 @@ export default function Trials(props: any) {
       </CardActions>
     </Card>
   );
+  const confirmRedistribute = (
+    <Card>
+      <CardContent className={classes.content}>
+        <div
+          style={{ display: 'flex', flexDirection: 'column', width: '100%' }}
+        >
+          <Typography
+            style={{ textAlign: 'center' }}
+            gutterBottom
+            variant="h5"
+            component="h2"
+          >
+            Are you sure? This process cannot be undone
+          </Typography>
+          <Typography
+            style={{ textAlign: 'center' }}
+            gutterBottom
+            color="textSecondary"
+            variant="body2"
+            component="p"
+          >
+            Redistributing a trial implies that all points in all rounds and groups will be deleted for this trial. You may not be able to replicate the last state. This process cannot be undone.
+          </Typography>
+        </div>
+      </CardContent>
+      <CardActions style={{ justifyContent: 'center' }}>
+        <Button
+          className={classes.action}
+          variant="contained"
+          size="small"
+          color="primary"
+          onClick={() => setActiveModal('')}
+        >
+          Cancel
+        </Button>
+        <Button
+          className={classes.action}
+          variant="contained"
+          size="small"
+          color="secondary"
+          onClick={() => redistributeTrial(currentId)}
+        >
+          Redistribute
+        </Button>
+      </CardActions>
+    </Card>
+  );
 
   const modalContent = (modalName) => {
     const modals = {
       confirmDelete,
+      confirmRedistribute,
     };
     return modals[modalName] || null;
   };
@@ -83,6 +131,8 @@ export default function Trials(props: any) {
     base
       .get('/managedTrialsList', { params })
       .then((r) => {
+        console.log(r.data);
+
         settrialsList(r.data);
       })
       .catch(() => {});
@@ -94,7 +144,17 @@ export default function Trials(props: any) {
 
   const deleteTrial = (id) => {
     base
-      .delete(`/deleteTrial/${id}`)
+      .delete(`/trial/${id}`)
+      .then((r) => {
+        setActiveModal('');
+        softRefresh();
+      })
+      .catch(() => {});
+  };
+
+  const redistributeTrial = (id) => {
+    base
+      .post(`/createBracketScore/${id}`)
       .then((r) => {
         setActiveModal('');
         softRefresh();
@@ -143,51 +203,68 @@ export default function Trials(props: any) {
             </Button>
           </CardActions>
         </Card>
-        {trialsList[0] && trialsList.map((content, i) => (
-          <div key={`TrialList${content.id}`} className={classes.options}>
-            <div className={classes.row}>
-              <div style={{ display: 'flex' }}>
+        {(trialsList[0] &&
+          trialsList.map((content, i) => (
+            <div key={`TrialList${content.id}`} className={classes.options}>
+              <div className={classes.row}>
+                <div style={{ display: 'flex' }}>
+                  <Typography
+                    component={'span'}
+                    style={{ margin: 0 }}
+                    gutterBottom
+                    variant="h6"
+                    color="textSecondary"
+                  >
+                    {content.id}.&nbsp;
+                  </Typography>
+                  <Typography
+                    component={'span'}
+                    style={{ margin: 0 }}
+                    gutterBottom
+                    variant="h6"
+                  >
+                    {content.name}
+                  </Typography>
+                </div>
                 <Typography
                   component={'span'}
                   style={{ margin: 0 }}
                   gutterBottom
-                  variant="h6"
+                  variant="body2"
                   color="textSecondary"
                 >
-                  {content.id}.&nbsp;
-                </Typography>
-                <Typography
-                  component={'span'}
-                  style={{ margin: 0 }}
-                  gutterBottom
-                  variant="h6"
-                >
-                  {content.name}
+                  {content.license_ido} - {content.motorcycle_plate}
                 </Typography>
               </div>
-              <Typography
-                component={'span'}
-                style={{ margin: 0 }}
-                gutterBottom
-                variant="body2"
-                color="textSecondary"
-              >
-                {content.license_ido} - {content.motorcycle_plate}
-              </Typography>
+              <div>
+                {content.type === 'bracket' ? (
+                  <Button
+                    className={classes.action}
+                    size="small"
+                    color="primary" 
+                    onClick={() => {
+                      setActiveModal('confirmRedistribute');
+                      setCurrentId(content.id);
+                    }}
+                  >
+                    Redistribute
+                  </Button>
+                ) : null}
+                <Button
+                  className={classes.action}
+                  size="small"
+                  color="secondary"
+                  onClick={() => {
+                    setActiveModal('confirmDelete');
+                    setCurrentId(content.id);
+                  }}
+                >
+                  Delete
+                </Button>
+              </div>
             </div>
-            <Button
-              className={classes.action}
-              size="small"
-              color="secondary"
-              onClick={() => {
-                setActiveModal('confirmDelete');
-                setCurrentId(content.id);
-              }}
-            >
-              Delete
-            </Button>
-          </div>
-        )) || 'there is no trial'}
+          ))) ||
+          'there is no trial'}
       </div>
       <Modal
         bodyStyle={{ margin: 'auto 20px', width: '100%' }}
